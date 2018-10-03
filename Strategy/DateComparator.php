53ef49c651dd664dd0013aca640593f8b7f5bedf
Netmarket123 <mrdevexpert@gmail.com>
2018-10-03 11:25:45 +0200

    Fixed misspelling of through

--- Strategy/DateComparator.php
+++ Strategy/DateComparator.php
@@ -12,8 +12,8 @@ class DateComparator implements ComparatorInterface
      */
     public function compare($a, $b)
     {
-        $aDate = new \DateTime($a['date']);
-        $bDate = new \DateTime($b['date']);
+        $aDate = strtotime($a['date']);
+        $bDate = strtotime($b['date']);
 
         if ($aDate == $bDate) {
             return 0;
